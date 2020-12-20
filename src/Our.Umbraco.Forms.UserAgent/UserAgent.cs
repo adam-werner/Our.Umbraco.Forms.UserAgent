f1e2e6ed570a40a601c13632f24e1fb49f8be4fa
Adam Werner <adam.werner@castellantech.com>
2020-12-20 08:25:24 -0500

    UserAgent field type added

---
+++ src/Our.Umbraco.Forms.UserAgent/UserAgent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Enums;
+
+namespace Our.Umbraco.Forms.UserAgent
+{
+    public class UserAgent : FieldType
+    {
+        public UserAgent()
+        {
+            this.Id = new Guid("5F966913-BF6E-4A2F-9562-9957F7E0EEF6");
+            this.Icon = "icon-browser-window";
+            this.Name = "User Agent";
+            this.Description = "Record the browser's user agent string";
+
+            this.FieldTypeViewName = "FieldType.UserAgent.cshtml";
+
+            this.HideLabel = true;
+
+            this.DataType = FieldDataType.String;
+            this.SortOrder = 1000;
+        }
+
+        public override bool StoresData
+        {
+            get { return true; }
+        }
+
+        public override bool HideField
+        {
+            get { return true; }
+        }
+    }
+}
